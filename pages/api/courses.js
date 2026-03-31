748bb1a2564eea7cbae7439170572ea1a31a7cfc
Rajeshnay <146355131+Rajeshnay@users.noreply.github.com>
2026-03-31 11:24:11 +0530

    move PDFs to public

--- pages/api/courses.js
+++ pages/api/courses.js
@@ -3,17 +3,17 @@ export default function handler(req, res) {
     {
       id: 1,
       name: "Course A",
-      pdf: "http://10.204.75.101:3000/api/files/abcd123.pdf",
+      pdf: "https://backend-tawny-seven-63.vercel.app/api/files/abcd123.pdf",
     },
     {
       id: 2,
       name: "Course B",
-      pdf: "http://10.204.75.101:3000/api/files/Psychology_.pdf",
+      pdf: "https://backend-tawny-seven-63.vercel.app/api/files/Psychology_.pdf",
     },
     {
       id: 3,
       name: "Course C",
-      pdf: "http://10.204.75.101:3000/api/files/vision_transformers.pdf",
+      pdf: "https://backend-tawny-seven-63.vercel.app/api/files/vision_transformers.pdf",
     },
   ]);
 }
